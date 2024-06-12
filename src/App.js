5d0fbb27d1c1913dd80c36f9eb32969a45c588e7
FoxS Local User <foxs5local@SeasideRZD.local>
2024-06-12 22:38:27 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     return (
     <div className="App">
       work
-      <button onClick={onClose}>Закрыть</button>
+        <button onClick={onClose}>Закрыть</button>
     </div>
   );
 }
